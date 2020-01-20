fb4650364ffd5d555417ed7c6140d3e025c84956
Glitch (billowy-parenthesis) <none>
2020-01-20 18:54:57 +0000

    👼🎍 Checkpoint ./routes/api.js:3976953/637 ./tests/2_functional-tests.js:3976953/427

--- routes/api.js
+++ routes/api.js
@@ -17,45 +17,52 @@ const MONGODB_CONNECTION_STRING = process.env.DB;
 
 module.exports = function (app) {
   
-MongoClient.connect(MONGODB_CONNECTION_STRING, {useUnifiedTopology: true}, function(err, client) {
-  if (err) console.log('Error connecting to DB');
-  var db = client.db('test');
-  
-  app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
-    .post(function (req, res){
-      var title = req.body.title;
-      if(!title) return res.send('Missing title');
-      //response will contain new book object including atleast _id and title
-    })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
-    });
+  MongoClient.connect(MONGODB_CONNECTION_STRING, {useUnifiedTopology: true}, function(err, client) {
+    if (err) console.log('Error connecting to DB');
+    var db = client.db('test');
+    var collection = db.collection('library');
+
+    app.route('/api/books')
+      .get(function (req, res){
+        //response will be array of book objects
+        //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      })
+
+      .post(function (req, res){
+        var title = req.body.title;
+        if(!title) return res.send('Missing title');
+        var book = {title: title, comments: []};
+        collection.insert(book, function (err, doc) {
+          if(err) console.log('Error posting book to library');
+          console.log(book);
+          return res.json(book);
+        });
+        //response will contain new book object including atleast _id and title
+      })
+
+      .delete(function(req, res){
+        //if successful response will be 'complete delete successful'
+      });
 
 
 
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      var bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-    })
-    
-    .post(function(req, res){
-      var bookid = req.params.id;
-      var comment = req.body.comment;
-      //json res format same as .get
-    })
-    
-    .delete(function(req, res){
-      var bookid = req.params.id;
-      //if successful response will be 'delete successful'
-    });
-  
-client.close();
-});
+    app.route('/api/books/:id')
+      .get(function (req, res){
+        var bookid = req.params.id;
+        //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      })
+
+      .post(function(req, res){
+        var bookid = req.params.id;
+        var comment = req.body.comment;
+        //json res format same as .get
+      })
+
+      .delete(function(req, res){
+        var bookid = req.params.id;
+        //if successful response will be 'delete successful'
+      });
+
+  client.close();
+  });
 };
